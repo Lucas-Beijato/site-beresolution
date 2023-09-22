184dc7ed1395a5612178197a817c025990a0ec7f
Lucas-Beijato <lucasbeijato0@gmail.com>
2023-09-22 19:42:19 -0300

    Correção de scripts do RD
    Alteração de forma de carregamento do componente do RD.

--- src/app/components/rdStationForm/index.tsx
+++ src/app/components/rdStationForm/index.tsx
@@ -4,8 +4,8 @@ const RdStationForm = () => {
   return (
     <>
       <div role="main" id="form-briefing-desenvolvimento-1e59238d9ca960ac0f6e"></div>
-      <Script strategy="beforeInteractive" type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></Script>
-      <Script strategy="beforeInteractive" type="text/javascript"> new RDStationForms('form-briefing-desenvolvimento-1e59238d9ca960ac0f6e', 'UA-256038179-1').createForm();</Script>
+      <Script strategy="lazyOnload" type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></Script>
+      <Script strategy="lazyOnload" type="text/javascript"> new RDStationForms('form-briefing-desenvolvimento-1e59238d9ca960ac0f6e', 'UA-256038179-1').createForm();</Script>
     </>
   )
 }
