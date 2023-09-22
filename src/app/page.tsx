86f49abfec01a593c61b42066fa0f0e52905c37d
Lucas-Beijato <lucasbeijato0@gmail.com>
2023-09-22 17:21:55 -0300

    Atualização final de deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Section1 from "./components/Home/section1/section1"
-import Section2 from "./components/Home/section2/section2"
-import Section3 from "./components/Home/section3/section3"
-import Section4 from "./components/Home/section4/section4"
-import Section5 from "./components/Home/section5/section5"
-import Section6 from "./components/Home/section6/section6"
+import Section1 from "./components/Home/section1"
+import Section2 from "./components/Home/section2"
+import Section3 from "./components/Home/section3"
+import Section4 from "./components/Home/section4"
+import Section5 from "./components/Home/section5"
+import Section6 from "./components/Home/section6"
 
 export default function Home() {
 
