7da58b79be5e2bcfcfd8d10581f6e95540596d49
Lucas-Beijato <lucasbeijato0@gmail.com>
2023-09-23 06:57:33 -0300

    Correção dos scripts RD
    Scripts estavam com problema, alterei a forma de carregamento para ver se resolve o problema de carregamento.

--- src/app/components/rdStationForm/index.tsx
+++ src/app/components/rdStationForm/index.tsx
@@ -4,7 +4,7 @@ const RdStationForm = () => {
   return (
     <>
       <div role="main" id="form-briefing-desenvolvimento-1e59238d9ca960ac0f6e"></div>
-      <Script strategy="lazyOnload" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></Script>
+      <Script strategy="afterInteractive" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></Script>
       <Script id="rdScript" strategy="lazyOnload">{`new RDStationForms('form-briefing-desenvolvimento-1e59238d9ca960ac0f6e', 'UA-256038179-1').createForm();`}</Script>
     </>
   )
