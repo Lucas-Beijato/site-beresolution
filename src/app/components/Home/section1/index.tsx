c88b988af1d0a64a46891df075d034003123dda5
Lucas-Beijato <lucasbeijato0@gmail.com>
2023-09-26 16:25:08 -0300

    Section 1 adaptada ao mobile

--- src/app/components/Home/section1/index.tsx
+++ src/app/components/Home/section1/index.tsx
@@ -5,12 +5,12 @@ import Section from "@/app/components/containerSection"
 export default function Section1() {
   return (
     <Section>
-      <div id="inicio" className="flex flex-col xl:w-[900px] pt-40 pb-5">
-        <h2 className="text-BeGreenColor font-bold text-2xl">UM POUCO SOBRE NÓS</h2>
+      <div id="inicio" className="flex flex-col xl:w-[900px] pt-10 xl:pt-40 pb-5">
+        <h2 className="text-BeGreenColor font-bold text-xl text-center xl:text-2xl">UM POUCO SOBRE NÓS</h2>
         <p className="text-white text-justify text-lg">Somos um time de profissionais apaixonados por tecnologia e aprimoramento de sistemas. Nosso DNA é impulsionado pelo desenvolvimento contínuo e aperfeiçoamento do nosso ERP SIGE. Nossa estrutura inclui departamentos de desenvolvimento, implantação e suporte, garantindo um produto abrangente e adaptável às necessidades de mais de 250 empresas em mais de 13 estados.  Oferecemos gestão eficiente e personalizada para impulsionar nossos clientes.</p>
       </div>
-      <Link className="bg-BeGreenColor shadow-lg w-fit py-2 rounded-lg px-4 mb-40 text-2xl font-semibold text-white transition transform duration-150 hover:scale-105" href="#atividades">Atividades</Link>
-      <Link className="text-BeGreenColor transition transform duration-150 hover:text-white text-xl flex flex-row justify-center items-center mb-4" href="#porqueNossoERP"><FiArrowDown />VER MAIS</Link>
+      <Link className="bg-BeGreenColor shadow-lg w-fit px-2 py-2 rounded-lg xl:px-4 mb-20 xl:mb-40 text-xl xl:text-2xl font-semibold text-white transition transform duration-150 hover:scale-105" href="#atividades">Atividades</Link>
+      <Link className="text-BeGreenColor transition transform duration-150 hover:text-white text-lg xl:text-xl flex flex-row justify-center items-center mb-4" href="#porqueNossoERP"><FiArrowDown />VER MAIS</Link>
     </Section>
   )
 }
--- src/app/components/containerSection/index.tsx
+++ src/app/components/containerSection/index.tsx
@@ -9,7 +9,7 @@ interface SectionProps {
 export default function Section ({ children, bgWhite, removePaddingX }: SectionProps) {
   return (
     <section className={`flex flex-col w-full ${ bgWhite && 'bg-white' } justify-start items-center`}>
-      <div className={`flex flex-col w-full xl:w-mediunScreen px-3 xl:py-20 ${ !removePaddingX && ' xl:px-20' } justify-center items-center`}>
+      <div className={`flex flex-col w-full xl:w-mediunScreen px-3 py-6 xl:py-20 ${ !removePaddingX && ' xl:px-20' } justify-center items-center`}>
         { children }
       </div>
     </section>
