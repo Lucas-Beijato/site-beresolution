4068320d44a9ada92251dac2dbacf2e399c9e1d8
Lucas-Beijato <lucasbeijato0@gmail.com>
2023-09-28 11:32:31 -0300

    Atualização vídeo home

--- src/app/components/Home/section1/index.tsx
+++ src/app/components/Home/section1/index.tsx
@@ -4,13 +4,19 @@ import Section from "@/app/components/containerSection"
 
 export default function Section1() {
   return (
-    <Section>
-      <div id="inicio" className="flex flex-col xl:w-[900px] pt-10 xl:pt-40 pb-5 scroll-mt-52">
-        <h2 className="text-BeGreenColor font-bold text-xl text-center xl:text-2xl">UM POUCO SOBRE NÓS</h2>
-        <p className="text-white text-justify text-lg">Somos um time de profissionais apaixonados por tecnologia e aprimoramento de sistemas. Nosso DNA é impulsionado pelo desenvolvimento contínuo e aperfeiçoamento do nosso ERP SIGE. Nossa estrutura inclui departamentos de desenvolvimento, implantação e suporte, garantindo um produto abrangente e adaptável às necessidades de mais de 250 empresas em mais de 13 estados.  Oferecemos gestão eficiente e personalizada para impulsionar nossos clientes.</p>
-      </div>
-      <Link className="bg-BeGreenColor shadow-lg w-fit px-2 py-2 rounded-lg xl:px-4 mb-20 xl:mb-40 text-xl xl:text-2xl font-semibold text-white hoverGlowUp" href="#atividades">Atividades</Link>
-      <Link className="text-BeGreenColor transition transform duration-150 hover:text-white text-lg xl:text-xl flex flex-row justify-center items-center mb-4" href="#porqueNossoERP"><FiArrowDown />VER MAIS</Link>
-    </Section>
+    <div className="w-full h-fit overflow-hidden">
+      <video className="videoHome" autoPlay loop muted>
+        <source src="/videos/videoHome.mp4" />
+      </video>
+      <Section>
+        <div id="inicio" className="flex flex-col z-10 xl:w-[900px] pt-10 xl:pt-40 pb-5 scroll-mt-52">
+          <h2 className="text-BeGreenColor font-bold text-xl text-center xl:text-2xl">UM POUCO SOBRE NÓS</h2>
+          <p className="text-white text-justify text-lg">Somos um time de profissionais apaixonados por tecnologia e aprimoramento de sistemas. Nosso DNA é impulsionado pelo desenvolvimento contínuo e aperfeiçoamento do nosso ERP SIGE. Nossa estrutura inclui departamentos de desenvolvimento, implantação e suporte, garantindo um produto abrangente e adaptável às necessidades de mais de 250 empresas em mais de 13 estados.  Oferecemos gestão eficiente e personalizada para impulsionar nossos clientes.</p>
+        </div>
+        <Link className="bg-BeGreenColor shadow-lg w-fit px-2 py-2 rounded-lg xl:px-4 mb-20 xl:mb-40 text-xl xl:text-2xl font-semibold text-white hoverGlowUp" href="#atividades">Atividades</Link>
+        <Link className="text-BeGreenColor transition transform duration-150 hover:text-white text-lg xl:text-xl flex flex-row justify-center items-center mb-4" href="#porqueNossoERP"><FiArrowDown />VER MAIS</Link>
+      </Section>
+    </div>
+
   )
 }
