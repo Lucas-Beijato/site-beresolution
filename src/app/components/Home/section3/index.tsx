098b12bb6847a11465044ea9d044648968912d4a
Lucas-Beijato <lucasbeijato0@gmail.com>
2023-10-20 10:09:22 -0300

    Alteração de cores do background
    Cezar me pediu para trocar as cores, então foi feito, alterei as imagens e cores.

--- src/app/components/Home/section3/index.tsx
+++ src/app/components/Home/section3/index.tsx
@@ -16,49 +16,49 @@ import { BiSolidSticker } from "react-icons/bi"
 export default function Section3() {
 
   return (
-    <Section removePaddingX imageBG={{ imageColor: 'Gray', chosenImage: 1 }}>
-      <Title id="atividades" title={"Atividades"} subtitle={"SETORES QUE ATUAMOS"} />
+    <Section removePaddingX imageBG={{ imageColor: 'White', chosenImage: 1 }}>
+      <Title id="atividades" colorTitleDark title={"Atividades"} subtitle={"SETORES QUE ATUAMOS"} />
       <div className="flex xl:w-[950px] flex-row flex-wrap justify-center xl:justify-between items-start pt-5">
 
         <div className="flex w-72 flex-wrap justify-between">
 
           <Link className="w-full h-full" href={"/meu-negocio/industria"}>
-            <div className="bg-white w-full h-32 rounded-xl my-2 flex justify-center items-center hoverGlowUp">
-              <h2 className="text-BeColorBG font-bold text-2xl">Industria</h2>
+            <div className="bg-BeColorBG w-full h-32 rounded-xl my-2 flex justify-center items-center hoverGlowUp">
+              <h2 className="text-white font-bold text-2xl">Industria</h2>
             </div>
           </Link>
 
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiMetalBar className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Metal Mecânica</h2>
+            <h2 className="font-bold text-center leading-none text-white">Metal Mecânica</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiOpenedFoodCan className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Alimentícia</h2>
+            <h2 className="font-bold text-center leading-none text-white">Alimentícia</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiMicrochip className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Produtos eletrônicos</h2>
+            <h2 className="font-bold text-center leading-none text-white">Produtos eletrônicos</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiRobotLeg className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Implantes ortopédicos</h2>
+            <h2 className="font-bold text-center leading-none text-white">Implantes ortopédicos</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiChemicalDrop className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Química</h2>
+            <h2 className="font-bold text-center leading-none text-white">Química</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <BiSolidSticker className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Borracha</h2>
+            <h2 className="font-bold text-center leading-none text-white">Borracha</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <TbTransform className="h-12 w-12 fill-BeGreenColor stroke-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Extrativista e transformação</h2>
+            <h2 className="font-bold text-center leading-none text-white">Extrativista e transformação</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiMeltingMetal className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Beneficiamento matalúrgico</h2>
+            <h2 className="font-bold text-center leading-none text-white">Beneficiamento matalúrgico</h2>
           </div>
 
         </div>
@@ -66,38 +66,38 @@ export default function Section3() {
         <div className="flex w-72 flex-wrap justify-between">
 
           <Link className="w-full h-full" href={"/meu-negocio/distribuicao"}>
-            <div className="bg-white w-full h-32 rounded-xl my-2 flex justify-center items-center hoverGlowUp">
-              <h2 className="text-BeColorBG font-bold text-2xl">Distribuição</h2>
+            <div className="bg-BeColorBG w-full h-32 rounded-xl my-2 flex justify-center items-center hoverGlowUp">
+              <h2 className="text-white font-bold text-2xl">Distribuição</h2>
             </div>
           </Link>
 
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiMouse className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Suprimentos de informática</h2>
+            <h2 className="font-bold text-center leading-none text-white">Suprimentos de informática</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiGears className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Peças automotivas</h2>
+            <h2 className="font-bold text-center leading-none text-white">Peças automotivas</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiLiquidSoap className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Cosméticos</h2>
+            <h2 className="font-bold text-center leading-none text-white">Cosméticos</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiStethoscope className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Produtos Hospitalares</h2>
+            <h2 className="font-bold text-center leading-none text-white">Produtos Hospitalares</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiWoodBeam className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Produtos para marcenaria</h2>
+            <h2 className="font-bold text-center leading-none text-white">Produtos para marcenaria</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiBrickPile className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Materiais para construção</h2>
+            <h2 className="font-bold text-center leading-none text-white">Materiais para construção</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <ImPowerCord className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Materiais elétricos</h2>
+            <h2 className="font-bold text-center leading-none text-white">Materiais elétricos</h2>
           </div>
 
         </div>
@@ -105,34 +105,34 @@ export default function Section3() {
         <div className="flex w-72 flex-wrap justify-between">
 
           <Link className="w-full h-full" href={"/meu-negocio/servicos"}>
-            <div className="bg-white w-full h-32 rounded-xl my-2 flex justify-center items-center hoverGlowUp">
-              <h2 className="text-BeColorBG font-bold text-2xl">Serviços</h2>
+            <div className="bg-BeColorBG w-full h-32 rounded-xl my-2 flex justify-center items-center hoverGlowUp">
+              <h2 className="text-white font-bold text-2xl">Serviços</h2>
             </div>
           </Link>
 
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiSolarPower className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Energia Solar</h2>
+            <h2 className="font-bold text-center leading-none text-white">Energia Solar</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <FaUserTie className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Prestadores de serviços</h2>
+            <h2 className="font-bold text-center leading-none text-white">Prestadores de serviços</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiShop className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Comércio varejista</h2>
+            <h2 className="font-bold text-center leading-none text-white">Comércio varejista</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <TbPackages className="h-12 w-12 fill-white stroke-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Comércio atacadista</h2>
+            <h2 className="font-bold text-center leading-none text-white">Comércio atacadista</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GiShipBow className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Importadoras</h2>
+            <h2 className="font-bold text-center leading-none text-white">Importadoras</h2>
           </div>
-          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-white rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
+          <div className="flex flex-col w-AtividadesMiniCards h-AtividadesMiniCards bg-BeColorBG rounded-xl my-2 justify-center p-2 items-center hoverGlowUp">
             <GoPackageDependents className="h-12 w-12 fill-BeGreenColor" />
-            <h2 className="font-bold text-center leading-none text-BeColorBG">Revendedoras</h2>
+            <h2 className="font-bold text-center leading-none text-white">Revendedoras</h2>
           </div>
 
         </div>
