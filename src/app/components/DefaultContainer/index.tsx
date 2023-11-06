31f214e35584fd193bd2a4ea662616e50cd98c28
Lucas-Beijato <lucasbeijato0@gmail.com>
2023-11-06 14:45:02 -0300

    Update componente de Imagens, section 2
    Removido e substituído por tags img

--- src/app/components/DefaultContainer/index.tsx
+++ src/app/components/DefaultContainer/index.tsx
@@ -1,15 +1,16 @@
-import { HTMLAttributes, ReactNode } from "react";
+import { ReactNode } from "react";
 
 interface DefaultSectionProp {
   children: ReactNode,
   removePaddingX?: boolean,
   imgURL?: string,
-  imgCustomStyle?: string
+  imgCustomStyle?: string,
+  colorDark?: boolean
 }
 
-export default function DefaultSection({ children, removePaddingX, imgURL, imgCustomStyle }: DefaultSectionProp) {
+export default function DefaultSection({ children, removePaddingX, imgURL, imgCustomStyle, colorDark }: DefaultSectionProp) {
   return (
-    <section className={`overflow-hidden z-[0] w-full relative`}>
+    <section className={`overflow-hidden z-[0] w-full relative ${colorDark ? 'bg-BeColorBG': ''}`}>
       {imgURL && <img className={`${imgCustomStyle} absolute z-[1]`} loading="lazy" src={imgURL} alt="Fundo da seção." /> }
       <div className="flex justify-center items-center z-10">
         <div className={`flex flex-col w-full xl:w-mediunScreen px-3 py-6 xl:py-20 ${!removePaddingX && ' xl:px-20'} justify-center items-center`}>{children}</div>
--- src/app/components/Home/section2/index.tsx
+++ src/app/components/Home/section2/index.tsx
@@ -1,21 +1,12 @@
-import Image from "next/image"
 import Title from "@/app/components/title"
 import DefaultSection from "../../DefaultContainer";
-// -------------
-import industriaImg from '../../../../../public/imgs/industria-250px.jpg'
-import distribuicaoImg from '../../../../../public/imgs/distribuicao-250px.jpg'
-import servicosImg from '../../../../../public/imgs/servico-250px.jpg'
-import mobilidadeImg from '../../../../../public/imgs/mobilidade-250px.jpg'
-import inteligenciaNegociosImg from '../../../../../public/imgs/inteligencia_negocios-250px.jpg'
-import logoSIEGE from '../../../../../public/logos/logo siege.png'
-// -------------
 
 export default function Section2() {
   return (
 
     <DefaultSection
       removePaddingX
-      imgCustomStyle="w-[200px] h-[200px] top-[100px] left-[] opacity-[]"
+      imgCustomStyle="h-[100%] top-[100px] left-[] opacity-[40%]"
       imgURL="/imgs/figure_contact.svg"
     >
 
@@ -24,7 +15,7 @@ export default function Section2() {
       <div className="flex h-fit xl:h-[331px] flex-row xl:justify-between justify-center mt-10 w-full flex-wrap z-[1]">
 
         <div className="flex box-border flex-col my-1 xl:my-0 shadow-lg h-full hover:drop-shadow-model1 transition duration-150 transform w-full xl:w-250px rounded-lg bg-BeColorBG hoverGlowUp">
-          <Image className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" width='200' height='200' src={industriaImg} alt={"Icone card sobre setor Industrial"} />
+          <img className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" src="/imgs/industria-250px.jpg" alt={"Icone card sobre setor Industrial"} />
           <div className="px-3">
             <h3 className="text-white font-bold text-xl xl:h-[60px]">1 - Industrial</h3>
             <p className="text-white text-justify text-sm pb-3 h-fit">Gerencie seu processo produtivo com nosso PCP (Planejamento e controle da produção)</p>
@@ -32,7 +23,7 @@ export default function Section2() {
         </div>
 
         <div className="flex box-border flex-col my-1 xl:my-0 shadow-lg h-full hover:drop-shadow-model1 transition duration-150 transform w-full xl:w-250px rounded-lg bg-BeColorBG hoverGlowUp">
-          <Image className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" width='200' height='200' src={distribuicaoImg} alt={"Icone card sobre setor Industrial"} />
+          <img className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" src="/imgs/distribuicao-250px.jpg" alt={"Icone card sobre setor Industrial"} />
           <div className="px-3">
           <h3 className="text-white font-bold text-xl xl:h-[60px]">2 - Distribuição</h3>
             <p className="text-white text-justify text-sm pb-3 h-fit">Mensure números, analise desempenho de vendas e crie metas com nosso ERP.</p>
@@ -40,7 +31,7 @@ export default function Section2() {
         </div>
 
         <div className="flex box-border flex-col my-1 xl:my-0 shadow-lg h-full hover:drop-shadow-model1 transition duration-150 transform w-full xl:w-250px rounded-lg bg-BeColorBG hoverGlowUp">
-          <Image className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" width='200' height='200' src={servicosImg} alt={"Icone card sobre setor Industrial"} />
+          <img className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" src="/imgs/servico-250px.jpg" alt={"Icone card sobre setor Industrial"} />
           <div className="px-3">
           <h3 className="text-white font-bold text-xl xl:h-[60px]">3 - Serviços</h3>
             <p className="text-white text-justify text-sm pb-3 h-fit">Produtos e serviços em um só ERP, prático, seguro e ágil.</p>
@@ -48,7 +39,7 @@ export default function Section2() {
         </div>
 
         <div className="flex box-border flex-col my-1 xl:my-0 shadow-lg h-full hover:drop-shadow-model1 transition duration-150 transform w-full xl:w-250px rounded-lg bg-BeColorBG hoverGlowUp">
-          <Image className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" width='200' height='200' src={mobilidadeImg} alt={"Icone card sobre setor Industrial"} />
+          <img className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" src="/imgs/mobilidade-250px.jpg" alt={"Icone card sobre setor Industrial"} />
           <div className="px-3">
           <h3 className="text-white font-bold text-xl xl:h-[60px]">4 - Mobilidade</h3>
             <p className="text-white text-justify text-sm pb-3 h-fit">As operações de campo 100% integrada ao seu ERP.</p>
@@ -56,7 +47,7 @@ export default function Section2() {
         </div>
 
         <div className="flex box-border flex-col my-1 xl:my-0 shadow-lg h-full hover:drop-shadow-model1 transition duration-150 transform w-full xl:w-250px rounded-lg bg-BeColorBG hoverGlowUp">
-          <Image className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" width='200' height='200' src={inteligenciaNegociosImg} alt={"Icone card sobre setor Industrial"} />
+          <img className="mb-3 rounded-lg w-full h-[160px] max-h-[160px] xl:h-full object-cover" loading="lazy" src="/imgs/inteligencia_negocios-250px.jpg" alt={"Icone card sobre setor Industrial"} />
           <div className="px-3">
           <h3 className="text-white font-bold text-xl xl:h-[60px]">5 - Inteligência de Negócios</h3>
             <p className="text-white text-justify text-sm pb-3 h-fit">BI e Dashboard e uma única base dados. O poder da extração e análise de dados a favor do seu negócio.</p>
@@ -66,9 +57,7 @@ export default function Section2() {
       </div>
 
       <div className="flex w-full justify-center items-center z-10" >
-
-        <Image className="py-10" src={logoSIEGE} alt={"Logo Sistema SIEGE"} />
-
+        <img className="py-10" src="/logos/logo siege.png" alt={"Logo Sistema SIEGE"} />
       </div>
 
     </DefaultSection>
