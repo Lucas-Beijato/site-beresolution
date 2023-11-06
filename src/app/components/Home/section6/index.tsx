27e77fa94d88081a6fbaf7aac78c9cc76780960f
Lucas-Beijato <lucasbeijato0@gmail.com>
2023-11-06 20:09:42 -0300

    Removendo scripts de formulário
    Não vamos mais usar o Script de formulário do RD, aguardando a nova ferramenta para realizar a integração.

--- src/app/components/Home/section6/index.tsx
+++ src/app/components/Home/section6/index.tsx
@@ -1,5 +1,4 @@
 import Title from "@/app/components/title"
-import RdStationFormScript from "../../rdStationForm"
 import CustomSection from "../../CustomSection"
 
 export default function Section6() {
@@ -8,7 +7,9 @@ export default function Section6() {
       <div id="contato" className="flex flex-row w-full scroll-mt-52 flex-wrap">
         <div className="flex bg-white flex-col w-full xl:w-1/2 rounded-lg border-2 p-5 border-BeGreenColor">
           <Title colorTitleDark title={"Entre em contato"} subtitle={"FALE COM NOSSOS ESPECIALISTAS"} />
-          <RdStationFormScript/>
+
+          {/* Imprementar Formulário */}
+
         </div>
         <div className="flex flex-col w-full xl:w-1/2 p-5 ">
           <img className="w-full h-full" src="/imgs/figure_contact.svg" alt={"Figura ilustrando contato."} />
